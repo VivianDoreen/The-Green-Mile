cd4255b42468581010cdd1f740ba514528114226
VivianDoreen <nabulovivian2014@gmail.com>
2019-09-23 11:22:26 +0300

    add heroku database uri to database

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,7 +1,8 @@
 from database import DatabaseConnection
 import datetime
 connection = DatabaseConnection()
-response =[]
+response = []
+
 
 class UserModel():
     @staticmethod
@@ -16,33 +17,30 @@ class UserModel():
         try:
             query_to_check_for_username = "SELECT * FROM users WHERE username=%s"
             connection.cursor.execute(query_to_check_for_username, [username])
-            row_username=connection.cursor.fetchone()
+            row_username = connection.cursor.fetchone()
 
             query_to_check_for_email = "SELECT * FROM users WHERE email=%s"
             connection.cursor.execute(query_to_check_for_email, [email])
-            row_email=connection.cursor.fetchone()
-
-            
-
-            if not (row_username and row_email):
+            row_email = connection.cursor.fetchone()
+            if not (row_username or row_email):
                 register_user_query = "INSERT INTO users(name, email, username, password, date_created, date_modified) VALUES (%s, %s, %s, %s, %s, %s)"
-                connection.cursor.execute(register_user_query, [full_name, email, username, password, date_created, date_modified])
+                connection.cursor.execute(register_user_query, [
+                                          full_name, email, username, password, date_created, date_modified])
 
-                query_to_check_for_inserted_user= "SELECT * FROM users WHERE username=%s"
-                connection.cursor.execute(query_to_check_for_inserted_user, [username])
-                row=connection.cursor.fetchone()
+                query_to_check_for_inserted_user = "SELECT * FROM users WHERE username=%s"
+                connection.cursor.execute(
+                    query_to_check_for_inserted_user, [username])
+                row = connection.cursor.fetchone()
                 if not row:
                     return "No results to fetch"
                 response = {
-                    'id':row[0],
-                    'name':row[1],
-                    'email':row[2],
-                    'username':row[3],
-                    'password':row[4]
+                    'id': row[0],
+                    'name': row[1],
+                    'email': row[2],
+                    'username': row[3],
+                    'password': row[4]
                 }
                 return response
             return "Email or username already exists"
         except Exception as exc:
             print(exc)
-
-
--- app/views/index.py
+++ app/views/index.py
@@ -8,4 +8,4 @@ def index():
     Index route
     :return:
     """
-    return "<h2>Welcome to Green Mile</h2> The most efficient company in the whole world. Try us, you will see"
+    return "<h2>Welcome to Green Mile</h2> The most efficient & leading company in the whole world. Try us, you will see"
